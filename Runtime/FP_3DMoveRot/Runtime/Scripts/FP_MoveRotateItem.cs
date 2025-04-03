aed1d9dbcad528618bd002a2c9ccfa4300e4d426
John Shull <the.john.shull@gmail.com>
2025-04-03 08:59:09 -0400

    Pan / Move
    * Old Mouse System to Move 3D Items

---
+++ Runtime/FP_3DMoveRot/Runtime/Scripts/FP_MoveRotateItem.cs
@@ -0,0 +1,261 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FuzzPhyte.ThreeD
+{
+    /*
+     * private void AlignmentCheck()
+        {
+            if (potentialMate != null && correctPart &&!finalPlacement)
+            {
+                //check if we are aligned
+                //use cross product to check if we are aligned for euler rotation value
+                var potentialMateValue = Quaternion.Euler(potentialMate.gameObject.transform.rotation.eulerAngles);
+                Debug.LogWarning($"{this.gameObject.name}| potentialMate: {potentialMate.gameObject.name}, mate rotation = ({potentialMateValue.x},{potentialMateValue.y},{potentialMateValue.z},{potentialMateValue.w})");
+                var curRotation = Quaternion.Euler(this.GameData.EulerOrientation);
+                float angleDifference = Quaternion.Angle(potentialMateValue, curRotation);
+                //var dot = Vector3.Dot(potentialMate.gameObject.transform.rotation.eulerAngles, this.GameData.EulerOrientation);
+                var lowerBound = 0 - (potentialMate.GameData.AlignmentVariation*50);
+                var upperBound = 0 + (potentialMate.GameData.AlignmentVariation*50);
+                Debug.LogWarning($"{this.gameObject.name}| angleDiff: {angleDifference} lowerBound {lowerBound} upperBound {upperBound}");
+                
+                if (angleDifference >= lowerBound && angleDifference <= upperBound)
+                {
+                    correctOrientation = true;
+                    //force it to the right alignment
+                    potentialMate.gameObject.transform.rotation = Quaternion.Euler(this.GameData.EulerOrientation);
+                }
+                
+            }
+        }
+     */
+    public class FP_MoveRotateItem : MonoBehaviour
+    {
+        //this class is designed to manage item related restrictions like rotation and movement by type.
+        [Header("Requirements for Motion and Rotation")]
+        public Vector3 RotationSnap = new Vector3(15f,15f,15f);
+        public Vector3 MovementSnap = new Vector3(0.025f, 0.025f, 0.025f);
+        [Space]
+        public bool UseRotationSnap=false;
+        public bool XRotationLocked;
+        public bool YRotationLocked;
+        public bool ZRotationLocked;
+        [Space]
+        public bool UseMovementSnap=false;
+        public bool XMovementLocked;
+        public bool YMovementLocked;
+        public bool ZMovementLocked;
+
+        private Vector3 accumulatedDelta = Vector3.zero;
+        private Quaternion accumulatedRotation = Quaternion.identity;
+        public event Action<GameObject> OnMoveStartedEvent;
+        public event Action<GameObject> OnMoveEndEvent;
+        public event Action<GameObject> OnRotationEndEvent;
+
+        public void OnEnable()
+        {
+            OnMoveEndEvent = new Action<GameObject>((gameObject) => { });
+            OnMoveStartedEvent = new Action<GameObject>((gameObject) => { });
+            OnRotationEndEvent = new Action<GameObject>((gameObject) => { });
+        }
+        public void MoveStarted()
+        {
+            OnMoveStartedEvent.Invoke(this.gameObject);
+        }
+        public void MoveEnd()
+        {
+            OnMoveEndEvent.Invoke(this.gameObject);
+        }
+        #region Public Accessors
+        /// <summary>
+        /// The user is going to pass a next location in world coordinates
+        /// and we will move the item to that location
+        /// based on the restrictions set above.
+        /// Returns a world coordinate location
+        /// </summary>
+        /// <param name="passedLocation">world coordinates</param>
+        public Vector3 MoveItem(Vector3 passedLocation)
+        {
+            if (!UseMovementSnap)
+            {
+                //we aren't using our movement snap just return the location we were passed
+                return passedLocation;
+            }
+            Vector3 currentPosition = this.transform.position;
+            Vector3 deltaPosition = passedLocation - currentPosition;
+
+            // Accumulate delta movements
+            accumulatedDelta += deltaPosition;
+
+            // Snap movement
+            accumulatedDelta.x = TrySnapMovement(accumulatedDelta.x, MovementSnap.x);
+            accumulatedDelta.y = TrySnapMovement(accumulatedDelta.y, MovementSnap.y);
+            accumulatedDelta.z = TrySnapMovement(accumulatedDelta.z, MovementSnap.z);
+
+            // Apply movement restrictions
+            if (XMovementLocked) accumulatedDelta.x = currentPosition.x;
+            if (YMovementLocked) accumulatedDelta.y = currentPosition.y;
+            if (ZMovementLocked) accumulatedDelta.z = currentPosition.z;
+
+            // Calculate new location
+            Vector3 returnLocation = currentPosition + accumulatedDelta;
+            accumulatedDelta = Vector3.zero; // Reset accumulated delta
+
+            return returnLocation;
+        }
+
+        /// <summary>
+        /// I need to make sure that this is delta rotation and not just a full rotation
+        /// </summary>
+        /// <param name="passedEulerRotation"></param>
+        /// <returns></returns>
+        public Quaternion RotateItem(Vector3 passedEulerRotation)
+        {
+            Quaternion targetRotation = Quaternion.Euler(passedEulerRotation);
+            Quaternion currentRotation = transform.rotation;
+            Quaternion deltaRotation = targetRotation * Quaternion.Inverse(currentRotation);
+
+            // Accumulate delta rotations
+            accumulatedRotation *= deltaRotation;
+
+            // Check if the accumulated rotation meets the snapping criteria
+            Vector3 accumulatedEuler = accumulatedRotation.eulerAngles;
+            bool shouldSnap = Mathf.Abs(accumulatedEuler.x) >= RotationSnap.x ||
+                              Mathf.Abs(accumulatedEuler.y) >= RotationSnap.y ||
+                              Mathf.Abs(accumulatedEuler.z) >= RotationSnap.z;
+
+            if (shouldSnap)
+            {
+                // Apply snapping and restrictions
+                Debug.Log($"Rotation Snap");
+                Quaternion snappedAndRestrictedRotation = ApplyRotationSnappingAndRestrictions(accumulatedRotation, currentRotation);
+                accumulatedRotation = Quaternion.identity; // Reset only after snapping
+                return snappedAndRestrictedRotation;
+            }
+
+            // If not yet meeting the snap criteria, return the current rotation
+            OnRotationEndEvent.Invoke(this.gameObject);
+            Debug.Log($"{gameObject.name}: Rotating!");
+            return currentRotation;
+        }
+        /// <summary>
+        /// public accessor to just rotate the item by the local values
+        /// </summary>
+        public void RotateItemByLocalValues()
+        {
+            if (UseRotationSnap)
+            {
+                if(XRotationLocked)
+                {
+                    RotationSnap.x = 0;
+                }
+                if (YRotationLocked)
+                {
+                    RotationSnap.y = 0;
+                }
+                if (ZRotationLocked)
+                {
+                    RotationSnap.z = 0;
+                }
+                transform.Rotate(RotationSnap);
+                OnRotationEndEvent.Invoke(this.gameObject);
+                Debug.Log($"{gameObject.name}: Rotating!");
+            }
+            //var rotationReturn = RotateItem(RotationSnap);
+            //transform.rotation = rotationReturn;
+        }
+        public void RotateItemByLocalValuesInverse()
+        {
+            if (UseRotationSnap)
+            {
+                if (XRotationLocked)
+                {
+                    RotationSnap.x = 0;
+                }
+                if (YRotationLocked)
+                {
+                    RotationSnap.y = 0;
+                }
+                if (ZRotationLocked)
+                {
+                    RotationSnap.z = 0;
+                }
+                transform.Rotate(RotationSnap*-1f);
+            }
+        }
+        public void RotateItemByLocalValuesAndCamera(Camera raycastCam)
+        {
+            //raycast from the camera to this transform center point
+            //get the direction of the raycast
+            Debug.DrawRay(transform.position, Vector3.up, Color.yellow, 10f);
+            //UnityEngine.Ray throwRay = new UnityEngine.Ray(raycastCam.transform.position,Vector3.Normalize(transform.position - raycastCam.transform.position));
+            //UnityEngine.Ray forwardRay = new UnityEngine.Ray(raycastCam.transform.position, raycastCam.transform.forward);
+            //var angleOff = Vector3.SignedAngle(forwardRay.direction, throwRay.direction, Vector3.up);
+            //Debug.Log($"Forward Angle Signed: {angleOff}");
+            //Debug.DrawRay(throwRay.origin, throwRay.direction * 10, Color.red, 10f);
+            //Debug.DrawRay(forwardRay.origin,forwardRay.direction * 10, Color.blue, 10f);
+            /*
+            if (angleOff < 0)
+            {
+                RotateItemByLocalValuesInverse();
+            }
+            else
+            {
+                RotateItemByLocalValues();
+            }
+            */
+        }
+
+        #endregion
+        private float TrySnapMovement(float delta, float snapValue)
+        {
+            if (Mathf.Abs(delta) >= snapValue)
+            {
+                return Mathf.Round(delta / snapValue) * snapValue;
+            }
+            return 0;
+        }
+        
+
+        private Quaternion ApplyRotationSnappingAndRestrictions(Quaternion deltaRotation, Quaternion baseRotation)
+        {
+            Vector3 deltaEuler = deltaRotation.eulerAngles;
+
+            // Snap rotation
+            deltaEuler.x = TrySnapRotation(deltaEuler.x, RotationSnap.x);
+            deltaEuler.y = TrySnapRotation(deltaEuler.y, RotationSnap.y);
+            deltaEuler.z = TrySnapRotation(deltaEuler.z, RotationSnap.z);
+
+            // Apply rotation restrictions
+            Vector3 baseEuler = baseRotation.eulerAngles;
+            if (XRotationLocked) deltaEuler.x = baseEuler.x;
+            if (YRotationLocked) deltaEuler.y = baseEuler.y;
+            if (ZRotationLocked) deltaEuler.z = baseEuler.z;
+
+            // Combine base rotation and delta rotation
+            return Quaternion.Euler(baseEuler + deltaEuler);
+        }
+
+        private float TrySnapRotation(float delta, float snapValue)
+        {
+            // Ensure delta is within 0-360 range
+            delta = NormalizeAngle(delta);
+
+            if (Mathf.Abs(delta) >= snapValue)
+            {
+                return Mathf.Round(delta / snapValue) * snapValue;
+            }
+            return 0;
+        }
+
+        private float NormalizeAngle(float angle)
+        {
+            while (angle > 360) angle -= 360;
+            while (angle < 0) angle += 360;
+            return angle;
+        }
+
+    }
+}
---
+++ Runtime/FP_3DMoveRot/Runtime/Scripts/FP_PanMove.cs
@@ -0,0 +1,316 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FuzzPhyte.Ray;
+using System;
+
+namespace FuzzPhyte.ThreeD
+{
+    public class FP_PanMove : MonoBehaviour
+    {
+        public List<Collider> SnapPoints = new List<Collider>();
+        [SerializeField]
+        private List<Collider> UsedSnapPoints = new List<Collider>();
+        private Collider lastHitSnapPoint;
+        [SerializeField]
+        private GameObject selectedItem;
+        [SerializeField]
+        private FP_MoveRotateItem selectedItemDetails;
+        [SerializeField]
+        private Camera currentCam;
+        private Vector2 mouseScreenPos;
+        private Vector3 originalLocationOnActive;
+        /// <summary>
+        /// this we might adjust with other input information as needed
+        /// </summary>
+        private float originalDistanceFromCam;
+        private float zOffsetDistance = 0f;
+        public bool UseOffsetGridSnapping = true;
+        public float OffsetGridSize = 0.025f;
+        [SerializeField]
+        private bool isMoving;
+        [Tooltip("If we are further from this distance on drop we just return to original location")]
+        [Range(1,10)]
+        public float MaxDistanceBoundsCatch=4;
+        [Header("Movement Locked")]
+        public bool ManagerOverride;
+        public bool IsXLocked;
+        public bool IsYLocked;
+        public bool IsZLocked;
+        [Tooltip("Quick fix")]
+        public bool UseGameObjectPositionNotCollider;
+        public event Action<Collider,GameObject> OnSelectionEndAction;
+        
+        /// <summary>
+        /// Assuming something else is handling mouse information - we just need that :)
+        /// </summary>
+        /// <param name="passedMouseCor"></param>
+        public void UpdateMouseScreenPosition(Vector2 passedMouseCor)
+        {
+            //screen starts 0,0 bottom left
+            //screen ends Screen.width, Screen.height top right
+            if(!isMoving)
+            {
+                return;
+            }
+            mouseScreenPos = passedMouseCor;
+            //move item
+            if (selectedItem == null) return;
+
+            Vector3 calculatedPos = currentCam.ScreenToWorldPoint(new Vector3(mouseScreenPos.x, mouseScreenPos.y, originalDistanceFromCam+ zOffsetDistance));
+            var returnLocation = calculatedPos;
+            //get items localized location based on items information and restrictions
+            //we are then going to override that if we can or have the authority to do so
+            
+            //can do some override here and/or lock by our manager
+            //this will ignore the local items restrictions entirely for movement
+            //Apply axis lock if we have authority
+            if (ManagerOverride) 
+            {
+                //manager has authority 
+                //just need to check on our locked global axis.
+                if (IsXLocked) returnLocation.x = selectedItem.transform.position.x;
+                if (IsYLocked) returnLocation.y = selectedItem.transform.position.y;
+                if (IsZLocked) returnLocation.z = selectedItem.transform.position.z;
+            }
+            else
+            {
+                //individual item has authority
+                //use the on item restrictions to move the item
+                returnLocation=selectedItemDetails.MoveItem(calculatedPos);
+            }
+
+            selectedItem.transform.position = returnLocation;
+        }
+        public void Update()
+        {
+            if(!isMoving)
+            {
+                return;
+            }
+            UpdateMouseScreenPosition(Input.mousePosition);
+        }
+        
+        /// <summary>
+        /// tweak our z offset from another user input like forward/backward key
+        /// </summary>
+        /// <param name="passedZOffset"></param>
+        public void UpdateZOffset(float passedZOffset)
+        {
+            if (UseOffsetGridSnapping)
+            {
+                if (passedZOffset > 0)
+                {
+                    zOffsetDistance += OffsetGridSize;
+                    
+                }
+                else
+                {
+                    zOffsetDistance -= OffsetGridSize;
+                }
+                return;
+            }
+            //normal non grid snapping
+            
+            zOffsetDistance += passedZOffset;
+            
+        }
+        #region Action Based Functions
+        /// <summary>
+        /// public access for selection start based events
+        /// Coming in from the CC_Pickable Events
+        /// </summary>
+        /// <param name="item"></param>
+        public void SelectionStart(GameObject item)
+        {
+            if(item==null)
+            {
+                return;
+            }
+            if (!item.GetComponent<FP_MoveRotateItem>())
+            {
+                Debug.LogError($"Missing FP_MoveRotateItem on {item.name}");
+                return;
+            }
+            //set up our managers cached data by item and item data
+            selectedItem = item;
+            Debug.LogWarning($"Selected Item: {selectedItem.name}");
+            selectedItemDetails = item.GetComponent<FP_MoveRotateItem>();
+            originalLocationOnActive =item.transform.position;
+            //confirm camera
+            if(currentCam==null)
+            {
+                Debug.LogWarning($"No camera set: using main camera");
+                currentCam = Camera.main;
+            }
+            //set up our distance from camera
+            originalDistanceFromCam = Vector3.Distance(item.transform.position, currentCam.transform.position);
+            //fin
+            isMoving = true;
+            selectedItemDetails.MoveStarted();
+            //OnSelectionStartAction.Invoke(selectedItem);
+        }
+        /// <summary>
+        /// public access for selection ending based events
+        /// </summary>
+        public void SelectionEnd()
+        {
+            var nearPoint = FindNearestSnapPosition(selectedItem.transform.position);
+            Debug.Log($"Return Location:{nearPoint.Item1}, from {nearPoint.Item2.gameObject.name}");
+            selectedItem.transform.position = nearPoint.Item1;
+            Debug.Log($"SelectedItem.transform.position { selectedItem.gameObject.name}");
+            lastHitSnapPoint=nearPoint.Item2;
+            isMoving = false;
+            //snap to nearest snap point?
+            if(OnSelectionEndAction==null)
+            {
+                Debug.LogError($"Our OnSelectionEndAction apparently is null!?");
+            }else
+            {
+                if(lastHitSnapPoint!=null)
+                {
+                    OnSelectionEndAction.Invoke(lastHitSnapPoint,selectedItem);
+                }
+                else
+                {
+                    Debug.LogWarning($"no lastHitSnapPoint");
+                }
+            }
+           
+            if (selectedItemDetails != null)
+            {
+                selectedItemDetails.MoveEnd();
+            }
+            selectedItemDetails = null;
+            selectedItem = null;
+        }
+        #endregion
+        /// <summary>
+        /// This is coming in from Unlock sequences
+        /// </summary>
+        /// <param name="snapPoint"></param>
+        public void AddSnapPoint(Collider snapPoint)
+        {
+            if(!SnapPoints.Contains(snapPoint) && !UsedSnapPoints.Contains(snapPoint))
+            {
+                SnapPoints.Add(snapPoint);
+            }
+            
+        }
+        public void RemoveSnapPoint(Collider snapPointMatch)
+        {
+            if (SnapPoints.Contains(snapPointMatch))
+            {
+                SnapPoints.Remove(snapPointMatch);
+                UsedSnapPoints.Add(snapPointMatch);
+            }
+        }
+        /// <summary>
+        /// quick distance formula based on location of item in world space
+        /// </summary>
+        /// <param name="currentPosition"></param>
+        /// <returns></returns>
+        private (Vector3,Collider) FindNearestSnapPosition(Vector3 currentPosition)
+        {
+            //lets try seeing if we have a direct hit first
+            Collider potentialHit;
+            UnityEngine.Ray ray = currentCam.ScreenPointToRay(Input.mousePosition);
+            var foundMatch = TryGetSnapPointFromRaycast(ray, out potentialHit);
+            if (foundMatch)
+            {
+                if (Vector3.Distance(currentPosition, potentialHit.bounds.center) >= MaxDistanceBoundsCatch)
+                {
+                    /*
+                    if (UseGameObjectPositionNotCollider)
+                    {
+                        Debug.LogWarning($"Potential Hit Item Location:{potentialHit.gameObject.name}| {potentialHit.gameObject.transform.position}");
+                        return (potentialHit.gameObject.transform.position, potentialHit);
+                    }
+                    */
+                    Debug.LogWarning($"We have exceeded the MaxDistanceBoundsCatch! - return {originalLocationOnActive} via the potential hit {potentialHit}");
+                    return (originalLocationOnActive, potentialHit);
+                }
+                else
+                {
+                    if (UseGameObjectPositionNotCollider)
+                    {
+                        return (potentialHit.gameObject.transform.position, potentialHit);
+                    }
+                    else
+                    {
+                        return (potentialHit.bounds.center, potentialHit);
+                    }
+                }
+                
+               
+
+            }
+            
+            float minDistance = float.MaxValue;
+            Vector3 nearestSnapPosition = currentPosition;
+            Collider nearestSnapCollider = null;
+            for(int i = 0; i < SnapPoints.Count; i++)
+            {
+                var snapPoint = SnapPoints[i];
+                if (snapPoint != null)
+                {
+                    Vector3 snapCenter = snapPoint.bounds.center;
+                    float distance = Vector3.Distance(currentPosition, snapCenter);
+
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+                        if (UseGameObjectPositionNotCollider)
+                        {
+                            Debug.LogWarning($"SnapPoint Hit Location: {snapPoint.gameObject.name}| {snapPoint.gameObject.transform.position}");
+                            nearestSnapPosition = snapPoint.gameObject.transform.position;
+                        }
+                        else
+                        {
+                            nearestSnapPosition = snapCenter;
+                        }
+
+                        nearestSnapCollider = snapPoint;
+                    }
+                }
+            }
+            if (Vector3.Distance(currentPosition, nearestSnapPosition) >= MaxDistanceBoundsCatch)
+            {
+                return (originalLocationOnActive,nearestSnapCollider);
+            }
+            return (nearestSnapPosition,nearestSnapCollider);
+        }
+        /// <summary>
+        /// See if we find a snap point from a raycast thru the mouse position
+        /// </summary>
+        /// <param name="mousePosition"></param>
+        /// <param name="snapObject"></param>
+        /// <returns></returns>
+        private bool TryGetSnapPointFromRaycast(UnityEngine.Ray ray, out Collider snapObject)
+        {
+            Debug.LogWarning($"Try to get snap point from raycast");
+            var hitPts=Physics.RaycastAll(ray);
+            /*
+            foreach(var pt in hitPts)
+            {
+                Debug.Log($"Item: {pt.collider.name}");
+            }
+            */
+            if (hitPts.Length > 0)
+            {
+                foreach(var hitPt in hitPts)
+                {
+                    if (SnapPoints.Contains(hitPt.collider))
+                    {
+                        snapObject = hitPt.collider;
+                        
+                        return true;
+                    }
+                }
+            }
+
+            snapObject = null;
+            return false;
+        }
+    }
+}
